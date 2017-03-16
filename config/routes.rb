6df30ebfc05bdb886104474b162c373105f0d955
Kyle Skinner <kyleskin@gmail.com>
2017-03-16 17:10:10 -0600

    Users sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'static_pages#home'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
