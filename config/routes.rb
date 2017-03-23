630619f79de288f6df854a288b078927af65763e
Kyle Skinner <kyleskin@gmail.com>
2017-03-23 09:24:21 -0600

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   resources :users
 
   root 'static_pages#home'
 
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/login',   to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
