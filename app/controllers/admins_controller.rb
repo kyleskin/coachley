ac445fcab5ee3466dd8d8a3092668968fcb091b3
Kyle Skinner <kyleskin@gmail.com>
2017-03-27 15:33:31 -0600

    WIP

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,14 @@
 class AdminsController < ApplicationController
+  before_action :get_users, only:[:dashboard]
+
   def dashboard
 
   end
 
+
+  private
+
+  def get_users
+    @users = User.all
+  end
 end
--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    # @admin = Admin.create(name: "Admin Test", email: "admin@email.com", password: "foobar", password_confirmation: "foobar")
+    # @manager = Manager.create(name: "Manager Test", email: "manager@email.com", password: "foobar", password_confirmation: "foobar")
+    # @coach = Coach.create(name: "Coach Test", email: "coach@email.com", password: "foobar", password_confirmation: "foobar", manager_id: @manager.id)
+    # @rep = Rep.create(name: "Rep Test", email: "rep@email.com", password: "foobar", password_confirmation: "foobar", manager_id: @manager.id, coach_id: @coach.id)
+  end
+
   test "should get index" do
-    get admins_index_url
+    get admins_path
     assert_response :success
+  end
+
+  test "all users should be displayed on index" do
+    get admins_path
+    assert_select 'h2', "Admin Test"
+    assert_select 'h2', "Manager Test"
+    assert_select 'h2', "Coach Test"
+    assert_select 'h2', "Rep Test"
   end
 
 end
